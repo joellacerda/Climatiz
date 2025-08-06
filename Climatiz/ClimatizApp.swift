62bfc3df19f150aab1285d26f14d953d7fd0886a
Joel Lacerda <joellacerdaol@gmail.com>
2025-08-06 18:49:18 -0300

    Initial Commit

---
+++ Climatiz/ClimatizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimatizApp.swift
+//  Climatiz
+//
+//  Created by Joel Lacerda on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ClimatizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

20507a4e79e859eab0988d099495339e23e3572d
Joel Lacerda <joellacerdaol@gmail.com>
2025-08-06 19:55:35 -0300

    ✨ feat(ui): Add next day forecast and light blue color
    - Added a new light blue color asset for UI elements.
    - Implemented a NextDay view to display the forecast for the next days.
    - Integrated the NextDay view into the ContentView.

--- Climatiz/ContentView.swift
+++ Climatiz/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                colors: [.blue, .white],
+                colors: [.blue, .lightBlue],
                 startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             
@@ -31,6 +31,16 @@ struct ContentView: View {
                     Text("27°")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundStyle(.white)
+                }
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20) {
+                    NextDay(dayOfWeek: "QUA", imageName: "cloud.sun.fill", temperature: 29)
+                    NextDay(dayOfWeek: "QUI", imageName: "sun.max.fill", temperature: 30)
+                    NextDay(dayOfWeek: "SEX", imageName: "wind", temperature: 28)
+                    NextDay(dayOfWeek: "SAB", imageName: "cloud.fill", temperature: 29)
+                    NextDay(dayOfWeek: "DOM", imageName: "cloud.sun.fill", temperature: 27)
+                    
                 }
                 
                 Spacer()
---
+++ Climatiz/NextDay.swift
@@ -0,0 +1,32 @@
+//
+//  NextDay.swift
+//  Climatiz
+//
+//  Created by Joel Lacerda on 06/08/25.
+//
+
+import SwiftUI
+
+struct NextDay: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
+}
